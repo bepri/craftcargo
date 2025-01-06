951369a9623279fc4508c44163bd5f3de52c3368
Fabian Grünbichler <git@fabian.gruenbichler.email>
2025-01-06 09:36:48 +0100

    integration tests: don't require semver-suffix for config override
    with the recent changes of only providing the unversioned package names in
    non-semver-suffixed packages, and translating version ranges as upper and lower
    bound instead of alternatives, the latest version with an override might need
    to be non-semver-suffixed to be able to satisfy a version range dependency in
    an rdep.
    
    Signed-off-by: Fabian Grünbichler <git@fabian.gruenbichler.email>

--- tests/sh/integrate.sh
+++ tests/sh/integrate.sh
@@ -195,10 +195,6 @@ build_source() {(
 	local config="$config_dir/${deb_src_name}/debian/debcargo.toml"
 	if [ -f "$config" ]; then
 		option="--config $config"
-		if ! grep -q 'semver_suffix = true' "$config"; then
-			echo >&2 "bad config: $config must contain \"semver_suffix = true\""
-			return 1
-		fi
 		echo >&2 "using config: $config"
 	elif [ "$deb_src_name" != "$($debcargo deb-src-name "$crate" "")" ]; then
 		config="$config_dir/old-version/debian/debcargo.toml"
