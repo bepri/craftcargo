89541596186a4c658271228b7ede7235c17c2a43
Fabian Grünbichler <git@fabian.gruenbichler.email>
2025-02-28 19:49:47 +0100

    tests: actually call built debcargo binary
    this allows us to control the two pesky env variables, is less code, and
    actually tests what we want to test here.
    
    Signed-off-by: Fabian Grünbichler <git@fabian.gruenbichler.email>

--- tests/local-crate.rs
+++ tests/local-crate.rs
@@ -2,44 +2,26 @@ extern crate debcargo;
 
 use std::path::PathBuf;
 
-use debcargo::package::{PackageExecuteArgs, PackageExtractArgs, PackageInitArgs, PackageProcess};
-
 fn local_package_test(crate_name: &str, version: &str) -> String {
     let tempdir = tempfile::Builder::new()
         .prefix("debcargo")
         .tempdir_in(".")
         .expect("Should be able to create temporary directory");
-    let init_args = PackageInitArgs {
-        crate_name: crate_name.to_string(),
-        version: Some(version.to_string()),
-        config: Some(PathBuf::from(format!(
-            "tests/{crate_name}-overlay/debian/debcargo.toml"
-        ))),
-    };
-    let extract_args = PackageExtractArgs {
-        directory: Some(tempdir.path().join("output").to_owned()),
-    };
-    let execute_args = PackageExecuteArgs {
-        changelog_ready: false,
-        copyright_guess_harder: false,
-        no_overlay_write_back: true,
-    };
-    let mut process = PackageProcess::init(init_args).expect("Should be able to init packaging");
-    process
-        .extract(extract_args)
-        .expect("Should be able to extract crate");
-    process
-        .apply_overrides()
-        .expect("Should be able to apply overrides");
-    process
-        .prepare_orig_tarball()
-        .expect("Should be able to prepare orig tarball");
-    process
-        .prepare_debian_folder(execute_args)
-        .expect("Should be able to prepare debian/ dir");
-    process
-        .post_package_checks()
-        .expect("Post-package-checks shouldn't fail");
+    let output = std::process::Command::new(env!("CARGO_BIN_EXE_debcargo"))
+        .env("DEBFULLNAME", "Debcargo Test")
+        .env("DEBEMAIL", "debcargo@example.com")
+        .arg("package")
+        .arg("--config")
+        .arg(format!("tests/{crate_name}-overlay/debian/debcargo.toml"))
+        .arg("--directory")
+        .arg(tempdir.path().join("output"))
+        .arg("--no-overlay-write-back")
+        .arg(crate_name)
+        .arg(version)
+        .output()
+        .expect("Should be able to run `debcargo package`");
+
+    assert!(output.status.success());
 
     std::fs::read_to_string(tempdir.path().join("output/debian/control"))
         .expect("Should be able to read generated debian/control file")
